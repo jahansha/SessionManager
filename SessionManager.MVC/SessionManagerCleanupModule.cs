30e06713c9a66445f51e521527e3e62a512fd473
Jeremy Butts <jeremy>
2013-02-21 23:47:35 -0600

    Added Error Event to the Cleanup Module.

--- SessionManager.MVC/SessionManagerCleanupModule.cs
+++ SessionManager.MVC/SessionManagerCleanupModule.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using NHibernate;
+using System;
 using System.Collections.Generic;
 using System.Web;
 using System.Web.Mvc;
@@ -9,15 +10,17 @@ namespace SessionManager.MVC
     {
         private HttpApplication httpApplication;
 
-        public void Dispose()
-        {
-            httpApplication.EndRequest -= ContextEndRequest;
-        }
-
         public void Init(HttpApplication context)
         {
             httpApplication = context;
             context.EndRequest += ContextEndRequest;
+            context.Error += ContextError;
+        }
+
+        public void Dispose()
+        {
+            httpApplication.EndRequest -= ContextEndRequest;
+            httpApplication.Error -= ContextError;
         }
 
         private void ContextEndRequest(object sender, EventArgs e)
@@ -25,14 +28,27 @@ namespace SessionManager.MVC
             foreach (var sessionManager in GetSessionManagers())
             {
                 sessionManager.DisposeOfSession();
-            }            
-        }        
+            }
+        }  
+      
+        private void ContextError(object sender, EventArgs e)
+        {
+            var exception = HttpContext.Current.Server.GetLastError();
+
+            if (!(exception is HibernateException))
+            {
+                return;
+            }
+
+            foreach (var sessionManager in GetSessionManagers())
+            {
+                sessionManager.Rollback();
+            }
+        }
 
         private IEnumerable<ISessionManager> GetSessionManagers()
         {
-            var sessionManagers = DependencyResolver.Current.GetServices<ISessionManager>();
-
-            return sessionManagers ?? new List<ISessionManager>();
+            return DependencyResolver.Current.GetServices<ISessionManager>() ?? new List<ISessionManager>();
         }
     }
 }
