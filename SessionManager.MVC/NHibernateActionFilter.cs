63cf621e6f52db5ca70485ab10d615d75a670750
Jeremy Butts <jeremy>
2013-04-07 18:54:48 -0500

    Enhanced the NHibernateActionFilter
    Added the ability to specify if you would like the session injected into
    the SessionController.
    
    Added the ability to specify when you would like the session committed.

--- SessionManager.MVC/NHibernateActionFilter.cs
+++ SessionManager.MVC/NHibernateActionFilter.cs
@@ -1,43 +1,75 @@
-﻿using System.Web.Mvc;
+﻿using System;
+using System.Web.Mvc;
 
 namespace SessionManager.MVC
 {
+    public enum CommitOnType
+    {
+        OnActionExecuted,
+        OnResultExecuted
+    }
+    
     public class NHibernateActionFilter : ActionFilterAttribute
-    {   
+    {
+        private bool useSessionController = true;
+        private CommitOnType commitOn = CommitOnType.OnResultExecuted;
+
+        public bool UseSessionController { get { return useSessionController; } set { useSessionController = value; } }
+        public CommitOnType CommitOn { get { return commitOn; } set { commitOn = value; } }
+        
         public override void OnActionExecuting(ActionExecutingContext filterContext)
         {
-            var sessionController = filterContext.Controller as SessionController;
-
-            if (sessionController == null)
+            if (UseSessionController)
             {
-                return;
+                InjectSessionIntoController(filterContext);
             }
-
-            sessionController.Session = GetSessionManager().GetCurrentSession();
         }
 
         public override void  OnActionExecuted(ActionExecutedContext filterContext)
         {
-            if (filterContext.IsChildAction)
+            if (CommitOn == CommitOnType.OnActionExecuted)
+            {
+                var controllerContextWapper = new ControllerContextWrapper { ControllerContext = filterContext, Exception = filterContext.Exception, ExceptionHandled = filterContext.ExceptionHandled };
+                Commit(controllerContextWapper);
+            }  
+        }
+
+        public override void OnResultExecuted(ResultExecutedContext filterContext)
+        {
+            if (CommitOn == CommitOnType.OnResultExecuted)
+            {
+                var controllerContextWapper = new ControllerContextWrapper { ControllerContext = filterContext, Exception = filterContext.Exception, ExceptionHandled = filterContext.ExceptionHandled };
+                Commit(controllerContextWapper);
+            }
+        }
+
+        protected virtual void Commit(ControllerContextWrapper controllerContextWrapper)
+        {
+            if (controllerContextWrapper.ControllerContext.IsChildAction)
             {
                 return;
             }
 
-            if (filterContext.Exception != null &&
-                !filterContext.ExceptionHandled)
+            if (controllerContextWrapper.Exception != null &&
+                !controllerContextWrapper.ExceptionHandled)
             {
                 GetSessionManager().Rollback();
-                return;                
-            }
-            
-            var sessionController = filterContext.Controller as SessionController;
-
-            if (sessionController == null)
-            {
                 return;
-            }          
+            }
 
-            if (sessionController.ViewData.ModelState.IsValid)
+            var controller = controllerContextWrapper.ControllerContext.Controller;
+
+            if (UseSessionController)
+            {
+                controller = controllerContextWrapper.ControllerContext.Controller as SessionController;
+
+                if (controller == null)
+                {
+                    return;
+                }
+            }
+
+            if (controller.ViewData.ModelState.IsValid)
             {
                 GetSessionManager().Commit();
             }
@@ -47,9 +79,28 @@ namespace SessionManager.MVC
             }
         }
 
+        protected virtual void InjectSessionIntoController(ActionExecutingContext filterContext)
+        {
+            var controller = filterContext.Controller as SessionController;
+
+            if (controller == null)
+            {
+                return;
+            }
+
+            controller.Session = GetSessionManager().GetCurrentSession();
+        }
+
         protected virtual ISessionManager GetSessionManager()
         {
             return DependencyResolver.Current.GetService<ISessionManager>();
+        }
+
+        protected class ControllerContextWrapper
+        {
+            public ControllerContext ControllerContext { get; set; }
+            public Exception Exception { get; set; }
+            public bool ExceptionHandled { get; set; }
         }
     }
 }
